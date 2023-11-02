72be3559d818d7af27de78f3423dd8d99209bf70
catalinanimo <147264249+catalinanimo@users.noreply.github.com>
2023-11-02 20:19:50 -0300

    ddddd

---
+++ Teorica clases/Clasejava2.js
@@ -0,0 +1,19 @@
+// el for de java:
+for (let i=0 ; i<10; i++) {
+    console.log (i)
+}
+
+// el for x in f de python:
+
+let i = 0
+const estudiantes = ["Cata", "Agus", "Sol"]
+console.log (estudiantes.lenght)
+
+for (let i = 0; i <= estudiantes.length; i+1) {
+    let mail = (estudiantes[i] + '@itba.edu.ar')
+    
+}
+// no funcion
+
+
+
